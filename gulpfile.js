2edd4cbba3c0b8baebee9d7325ef1b1a641f17fd
abid <abidabdelmalek@gmail.com>
2021-04-23 19:07:24 +0100

    Add Header

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const {src,dest,watch} = require('gulp')
+const concat = require('gulp-concat')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const pug = require('gulp-pug')
+const livereload = require('gulp-livereload')
+const sourcemaps = require('gulp-sourcemaps')
+const minify = require('gulp-minify')
+
+// set pug
+function pugTask (){
+    return src('src/html/pug/*.pug')
+            .pipe(pug({
+                pretty: true
+            }))
+            .pipe(dest('dist'))
+            .pipe(livereload())
+}
+
+// set css
+function cssTask (){
+    return src(['src/css/**/*.css', 'src/css/**/*.scss'])
+            .pipe(sourcemaps.init())
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(autoprefixer())
+            .pipe(concat('main.css'))
+            .pipe(sourcemaps.write('.'))
+            .pipe(dest('dist/css'))
+            .pipe(livereload())
+}
+
+//set JS
+function jsTask(){
+    return src('src/js/*.js')
+            .pipe(concat('main.js'))
+            .pipe(minify())
+            .pipe(dest('dist/js'))
+            .pipe(livereload())
+}
+//set Watch
+function watcher(){
+   
+    livereload.listen()
+    watch('src/html/pug/**/*.pug', pugTask)
+    watch(['src/css/**/*.css', 'src/css/**/*.scss'], cssTask)
+    watch('src/js/*.js', jsTask)
+}
+module.exports = {
+    watch: watcher
+}
